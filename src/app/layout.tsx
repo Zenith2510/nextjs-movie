ab56ba7ee6de90c06bc789439ccd8890a67f19ec
Yan Myo Aung <yanmyoaung@Kyaws-MBP.hgu_lan>
2024-10-30 16:04:57 +0630

    fallback page incase of data fetching fail

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,8 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import Sidebar from "@/components/Sidebar";
 import Header from "@/components/Header";
+import { ErrorBoundary } from "next/dist/client/components/error-boundary";
+import Error from "./fallback/page";
 const inter = Inter({ subsets: ["latin"] });
 export const metadata = {
   title: "Next Movies",
@@ -16,16 +18,20 @@ export default function RootLayout({ children }: Props) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        <div className="mx-4 m-auto">
-          <Header />
-          <section className="flex gap-4 min-h-[500px] ">
-            <Sidebar />
-            <main className="border-l border-slate-300 px-4 ">{children}</main>
-          </section>
-          <footer className="text-center py-4 text-slate-400 mt-4 border-t border-slate-300">
-            <small className="text-xs">&copy; Copyright 2024</small>{" "}
-          </footer>
-        </div>{" "}
+        <ErrorBoundary errorComponent={Error}>
+          <div className="mx-4 m-auto">
+            <Header />
+            <section className="flex gap-4 min-h-[500px] ">
+              <Sidebar />
+              <main className="border-l border-slate-300 px-4 ">
+                {children}
+              </main>
+            </section>
+            <footer className="text-center py-4 text-slate-400 mt-4 border-t border-slate-300">
+              <small className="text-xs">&copy; Copyright 2024</small>{" "}
+            </footer>
+          </div>
+        </ErrorBoundary>
       </body>
     </html>
   );
